c00717cd9e6830394d0b478f54c182f5491eb73d
IroN404 <g.maitre2810@icloud.com>
2022-05-20 13:04:30 +0200

    création des classes telephone et categorie

--- modele/catalogue/models.py
+++ modele/catalogue/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class telephone(models.Model):
+    modele = models.CharField(max_length=100)
+    stockage = models.integer_field(blank=False)
+    date_sortie = models.DateField(blank=True, null=True)
+    details = models.text_field(blank=True, null=True)
+
+    def __str__(self):
+        chaine = f"{self.modele} {self.stockage} {self.date_sortie} {self.details}"
+        return chaine
+
+class categorie(models.model):
+    nom = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.nom
